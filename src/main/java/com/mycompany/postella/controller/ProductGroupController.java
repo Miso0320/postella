aa085ad318bf971c9d09eba82c958c7b5e807df1
pjhong1004 <pgc1004@gmail.com>
2023-08-09 17:37:48 +0900

    0809 : 페이징완료, 사진 몇개 추가

--- src/main/java/com/mycompany/postella/controller/ProductGroupController.java
+++ src/main/java/com/mycompany/postella/controller/ProductGroupController.java
@@ -42,56 +42,57 @@ public class ProductGroupController {
 
 	@RequestMapping("/productGroup")
 	public String getProductGroupList(String pageNo, Model model, HttpSession session) {
-				//pageNo가 넘어오지 않았을 경우
-				if(pageNo == null) {
-					//세션에 저장되어 있는지 확인
-					pageNo = (String) session.getAttribute("pageNo");
-					//저장되어 있지 않다면 "1"로 초기화
-					if (pageNo == null) {
-						pageNo = "1";
-					}
-				}
-				int intpageNo = Integer.parseInt(pageNo);
-				session.setAttribute("pageNo", String.valueOf(pageNo));
-				
-				int totalProductGroupNum = productGroupService.getTotalProductGroupNum();
-				
-				Pager pager = new Pager(12, 10, totalProductGroupNum, intpageNo);
-				List<ProductGroup> list = productGroupService.getList(pager);
-				
-				
-				
-				int pgNo;
-				Image img;
-				for(int i = 0; i < list.size(); i++) {
-					pgNo = list.get(i).getPg_no();
-					img = imageService.getImageByPgNo(pgNo);
-					String imgFile = Base64.getEncoder().encodeToString(img.getImg_file());
-					list.get(i).setEncodedFile(imgFile);
-					list.get(i).setImg_type(img.getImg_type());
-				}
-				
-				model.addAttribute("pager", pager);
-				model.addAttribute("productGroups", list);	
-				
-			/*{
-							int pgNo;
-							Image img;
-							for(int i = 0; i < list.size(); i++) {
-								pgNo = list.get(i).getPg_no();
-								img = imageService.getImageByPgNo(pgNo);
-								if(img != null) {
-									
-									//log.info("리스트 : " + list.get(i).getPg_no());
-									
-									String type = img.getImg_type();
-									String imgFile = Base64.getEncoder().encodeToString(img.getImg_file());
-									//log.info("가져온거 :" + img.getImg_type());		
-									
-									list.get(i).setEncodedFile(imgFile);
-									list.get(i).setImg_type(type);
-								}					
-						      }*/
+		//pageNo가 넘어오지 않았을 경우
+		if(pageNo == null) {
+			//세션에 저장되어 있는지 확인
+			pageNo = (String) session.getAttribute("pageNo");
+			//저장되어 있지 않다면 "1"로 초기화
+			if (pageNo == null) {
+				pageNo = "1";
+			}
+		}
+		int intpageNo = Integer.parseInt(pageNo);
+		session.setAttribute("pageNo", String.valueOf(pageNo));
+		
+		int totalProductGroupNum = productGroupService.getTotalProductGroupNum();
+		
+		Pager pager = new Pager(12, 10, totalProductGroupNum, intpageNo);
+		List<ProductGroup> list = productGroupService.getList(pager);
+		
+		
+		
+		int pgNo;
+		Image img = null;
+		for(int i = 0; i < list.size(); i++) {
+			pgNo = list.get(i).getPg_no();
+			img = imageService.getImageByPgNo(pgNo);
+			if(img != null) {
+				String imgFile = Base64.getEncoder().encodeToString(img.getImg_file());
+				list.get(i).setEncodedFile(imgFile);
+				list.get(i).setImg_type(img.getImg_type());
+			}
+		}			
+		model.addAttribute("pager", pager);
+		model.addAttribute("productGroups", list);	
+		
+	/*{
+					int pgNo;
+					Image img;
+					for(int i = 0; i < list.size(); i++) {
+						pgNo = list.get(i).getPg_no();
+						img = imageService.getImageByPgNo(pgNo);
+						if(img != null) {
+							
+							//log.info("리스트 : " + list.get(i).getPg_no());
+							
+							String type = img.getImg_type();
+							String imgFile = Base64.getEncoder().encodeToString(img.getImg_file());
+							//log.info("가져온거 :" + img.getImg_type());		
+							
+							list.get(i).setEncodedFile(imgFile);
+							list.get(i).setImg_type(type);
+						}					
+				      }*/
 		return "productGroup/productGroup";
 	}
 }
