c62342db1533095de361d4c718b92d66d5860685
Miso0320 <[miso8493@hanmail.net]>
2023-08-14 16:58:28 +0900

    230814 주문목록 검색기능 수정

--- src/main/java/com/mycompany/postella/controller/MypageController.java
+++ src/main/java/com/mycompany/postella/controller/MypageController.java
@@ -82,6 +82,8 @@ public class MypageController {
 		Date startDate = null;
 		Date endDate = null;
 		
+		log.info("지금연도는!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! : " + requestYear);
+		
         if(requestYear != null) {
         	if(requestYear.equals("2020year")) {
     			year = 2020;
@@ -121,6 +123,8 @@ public class MypageController {
             map.put("requestYear", requestYear);
             
         }
+        
+        log.info("목록 들어가기 직전 지금연도는!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! : " + requestYear);
 		
 		List<Orders> orders = myPageService.getOrderList(map);
 		
